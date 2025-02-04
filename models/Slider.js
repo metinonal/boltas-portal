39efc3b90a2a4e68b28976a694f327b1355f46ee
Metin Önal <91273044+metinonal@users.noreply.github.com>
2025-02-04 14:48:17 +0300

    update slider

--- models/Slider.js
+++ models/Slider.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const sliderSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String },
     imageUrl: { type: String, required: true },
     description: { type: String },
     count: {type: Number},
