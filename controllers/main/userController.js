34954700794665175184e598eb7c537f64804dc3
Metin Önal <91273044+metinonal@users.noreply.github.com>
2025-06-24 10:54:55 +0300

    zimmet eklendi

--- controllers/main/userController.js
+++ controllers/main/userController.js
@@ -1,10 +1,15 @@
-const { authMain } = require('../../middlewares/authMiddleware');
+const { authMain } = require("../../middlewares/authMiddleware")
+const { getPersonelZimmet } = require("../../models/userModel")
 
-const mainProfile = (req, res) => {
-  const user = req.session.user;
-  res.render('main/profile', { user });
-};
+const mainProfile = async (req, res) => {
+  try {
+    const user = req.session.user
+    const zimmetler = await getPersonelZimmet(user.EMail)
+    res.render("main/profile", { user, zimmetler })
+  } catch (error) {
+    console.error("Profil sayfası hatası:", error)
+    res.render("main/profile", { user: req.session.user, zimmetler: [] })
+  }
+}
 
-
-
-module.exports = { mainProfile };
+module.exports = { mainProfile }
--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,29 @@
-const { sql } = require('../config/config');
+const { sql } = require("../config/config")
 
 const getUserByEmail = async (email) => {
-  const request = new sql.Request();
-  request.input('email', sql.NVarChar, email);
+  const request = new sql.Request()
+  request.input("email", sql.NVarChar, email)
 
   try {
-    const result = await request.query('SELECT * FROM vHROrganizationFromOrtakIK_ALL WHERE EMail = @email');
-    return result.recordset[0];
+    const result = await request.query("SELECT * FROM vHROrganizationFromOrtakIK_ALL WHERE EMail = @email")
+    return result.recordset[0]
   } catch (err) {
-    console.error('MSSQL sorgu hatası:', err);
-    return null;
+    console.error("MSSQL sorgu hatası:", err)
+    return null
   }
-};
+}
 
-module.exports = { getUserByEmail };
+const getPersonelZimmet = async (email) => {
+  const request = new sql.Request()
+  request.input("email", sql.NVarChar, email)
+
+  try {
+    const result = await request.query("SELECT * FROM [dbo].[v_PersonelZimmet] WHERE PersonelMail = @email")
+    return result.recordset
+  } catch (err) {
+    console.error("MSSQL zimmet sorgu hatası:", err)
+    return []
+  }
+}
+
+module.exports = { getUserByEmail, getPersonelZimmet }
